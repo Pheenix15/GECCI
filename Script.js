c5db53ac32c1e5c2bbd631a5a5dcf855e0a90000
Pheenix15 <102440629+Pheenix15@users.noreply.github.com>
2023-11-26 20:14:04 +0100

    Add files via upload

--- Script.js
+++ Script.js
@@ -129,3 +129,10 @@ function tab3() {
 
 
 
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active-popup");
+}
+
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active-popup");
+}
